b9100355bd4911c4cf6edde7dfc333345f63694e
hrishi-19 <hrishakamath.com>
2022-11-11 14:04:45 +0530

    store enhancer and firestore setup

---
+++ src/config/fbConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAx_uAEXhMdW28bf0eQF-GCjNM_Lj7Q6U",
+  authDomain: "buddyplan-2aee8.firebaseapp.com",
+  projectId: "buddyplan-2aee8",
+  storageBucket: "buddyplan-2aee8.appspot.com",
+  messagingSenderId: "73387529220",
+  appId: "1:73387529220:web:2ac620ebff4a526ecc5151",
+  measurementId: "G-VQ8EDWB7S4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.firestore().settings({timestampsInSnapshots:true});
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore,applyMiddleware } from 'redux';
+import { createStore,applyMiddleware,compose } from 'redux';
 import rootReducer from './store/reducer/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { getFirebase,reactReduxFirebase } from 'react-redux-firebase';
+import { getFirestore,reduxFirestore } from 'redux-firestore';
+import firebase from './config/fbConfig';
 
 
-const store=createStore(rootReducer,applyMiddleware(thunk))
+
+const store=createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase)
+
+  ))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
